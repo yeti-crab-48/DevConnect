fa3e8bab569223d29494e04892553fe42696d6a7
NAusintO <nickaozawa@gmail.com>
2022-01-29 16:58:47 -0800

    progress with post card components

--- client/components/PostCard.jsx
+++ client/components/PostCard.jsx
@@ -8,16 +8,23 @@ const PostCard = ({key, id, title, createdAt}) => {
   return (
     console.log(title, createdAt), 
     <StyledPostCard key={key}>
-        {title}
+        <StyledTitle> {title} </StyledTitle>
         {createdAt}
     </StyledPostCard>
   )
 };
 
 const StyledPostCard = styled.div`
-width: 55px;
-height: 105px;
-border: 2px solid black; 
+width: 150px;
+height: 250px;
+overflow-y: scroll;
+border: 2px solid black;
+border-radius: 8px;
 `;
+
+const StyledTitle = styled.p`
+font-size: 16pt;
+font-weight: bold;
+`
 
 export default PostCard;
--- client/containers/Home.jsx
+++ client/containers/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { fetchPosts } from "../reducers/postReducer";
 import { connect } from "react-redux";
 import PostCard from "../components/PostCard";
+import styled from "styled-components";
+
 
 
 const mapStateToProps = (state) =>({
@@ -20,19 +22,26 @@ const mapDispatchToProps = (dispatch) =>({
 
 
 const Home = (props) => {
-
   useEffect(() => {
     props.getPosts();
   }, [])
 
   return (
-      
+
     <div>
       Hello this is home 
-      {props.postList.map((post, i) => { return  <PostCard title={post.title} createdAt={post.createdAt} key={i}/>})}
+      <PostCardWrapper>
+        {props.postList.map((post, i) => { return <PostCard title={post.title} createdAt={post.createdAt} key={i}/>})}
+      </PostCardWrapper>
     </div>
   )
 }
 
+const PostCardWrapper = styled.div`
+  display: flex;
+  column-gap: 30px;
+
+  margin: ;
+`;
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
